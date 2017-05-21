cb245d7747cd826a3f8e0953f6aea1de8deb7700
JFixby <github@jfixby.com>
2017-05-21 13:09:28 +0200

    android?

--- android/src/com/jfixby/bluemesa/AndroidLauncher.java
+++ android/src/com/jfixby/bluemesa/AndroidLauncher.java
@@ -1,16 +1,44 @@
+
 package com.jfixby.bluemesa;
 
-import android.os.Bundle;
+import java.io.IOException;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.jfixby.bluemesa.EntryPoint;
+
+import android.os.Bundle;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate (final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new EntryPoint(), config);
+		final AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		this.initialize(new EntryPoint(), config);
+
+		final Thread t = new Thread() {
+			@Override
+			public void run () {
+				try {
+					AndroidLauncher.this.test();
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		};
+		t.start();
+	}
+
+	private void test () throws IOException {
+		final String url = "btspp://98D331B2B6D3:1;authenticate=false;encrypt=false;master=false";
+		final GasSensorMessageReaderSpecs specs = new GasSensorMessageReaderSpecs();
+		specs.url = (url);
+		final GasSensorMessageReader reader = new GasSensorMessageReader(specs);
+		reader.open();
+
+		while (true) {
+			final GasSensorMessage message = reader.read();
+			message.print();
+		}
 	}
 }
