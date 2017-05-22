ae8225db01e9e436c81655718b141e9b2d001a1c
JFixby <github@jfixby.com>
2017-05-22 15:14:15 +0200

    x4

--- BlueMesa-android/src/com/jfixby/bluemesa/AnroidMessageTransport.java
+++ BlueMesa-android/src/com/jfixby/bluemesa/AnroidMessageTransport.java
@@ -3,15 +3,22 @@ package com.jfixby.bluemesa;
 
 import com.jfixby.bluemesa.sqs.MessageTransport;
 import com.jfixby.bluemesa.sqs.MessageTransportSpecs;
+import com.jfixby.bluemesa.sqs.MessagesConsumer;
+import com.jfixby.scarabei.api.json.Json;
 
 public class AnroidMessageTransport implements MessageTransport {
 
-	public AnroidMessageTransport (final MessageTransportSpecs t_specs) {
+	private final MessagesConsumer ep;
+
+	public AnroidMessageTransport (final MessageTransportSpecs t_specs, final MessagesConsumer ep) {
+		this.ep = ep;
 	}
 
 	@Override
 	public void send (final GasSensorMessage message) {
 		message.print();
+		final String messageText = Json.serializeToString(message).toString();
+		this.ep.append(messageText);
 	}
 
 }
